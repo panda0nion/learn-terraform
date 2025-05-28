608bae5ad3eab83e75ec363ff0d76eae314d4676
Patrick Lee <user-name@github.com>
2025-05-28 13:07:55 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "panda0nion"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
